3084970361b85a435aba2b409f01ebaf94dd6351
larry <guyko91@gmail.com>
2024-09-21 16:26:21 +0900

    [fix] : bug fix with OOP

--- src/main/java/cleancode/minesweeper/tobe/GameApplication.java
+++ src/main/java/cleancode/minesweeper/tobe/GameApplication.java
@@ -3,13 +3,14 @@ package cleancode.minesweeper.tobe;
 import cleancode.minesweeper.tobe.minesweeper.Minesweeper;
 import cleancode.minesweeper.tobe.minesweeper.config.GameConfig;
 import cleancode.minesweeper.tobe.minesweeper.gamelevel.Middle;
+import cleancode.minesweeper.tobe.minesweeper.gamelevel.VeryBeginner;
 import cleancode.minesweeper.tobe.minesweeper.io.ConsoleInputHandler;
 import cleancode.minesweeper.tobe.minesweeper.io.ConsoleOutputHandler;
 
 public class GameApplication {
 
     public static void main(String[] args) {
-        GameConfig gameConfig = GameConfig.of(new Middle(), new ConsoleInputHandler(), new ConsoleOutputHandler());
+        GameConfig gameConfig = GameConfig.of(new VeryBeginner(), new ConsoleInputHandler(), new ConsoleOutputHandler());
 
         Minesweeper minesweeperGame = new Minesweeper(gameConfig);
         minesweeperGame.initialize();
--- src/main/java/cleancode/minesweeper/tobe/minesweeper/board/cell/CellState.java
+++ src/main/java/cleancode/minesweeper/tobe/minesweeper/board/cell/CellState.java
@@ -26,10 +26,6 @@ public class CellState {
         return isOpened;
     }
 
-    public boolean isChecked() {
-        return this.isFlagged || this.isOpened;
-    }
-
     public boolean isFlagged() {
         return isFlagged;
     }
