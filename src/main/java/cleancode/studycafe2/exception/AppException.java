07b20f5d81efc57cf2e10af16c3ea4fcd2fbde21
larry <guyko91@gmail.com>
2024-09-22 20:24:21 +0900

    [refactor] : 중복제거, 메서드 추출

--- src/main/java/cleancode/studycafe2/exception/AppException.java
+++
@@ -1,9 +0,0 @@
-package cleancode.studycafe2.exception;
-
-public class AppException extends RuntimeException {
-
-    public AppException(String message) {
-        super(message);
-    }
-
-}
--- src/main/java/cleancode/studycafe2/machine/Machine.java
+++
@@ -1,7 +0,0 @@
-package cleancode.studycafe2.machine;
-
-public interface Machine {
-
-    void run();
-
-}
